7da759f65e8ad39fc6c0d81246e2940468c3893d
Emma <t3taem00@students.oamk.fi>
2025-09-03 14:09:16 +0300

    Todo4 valmis

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const url = "http://localhost:3001"
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
 
+
+
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setTasks(response.data)
+    })
+    .catch(error => {
+      alert(error.response.data ? error.response.data.message : error)
+    }) 
+  },[])
+
+
   const addTask = () => {
-    setTasks([...tasks,task])
-    setTask('')
+    const newTask = { description: task }
+
+    axios.post(url + "/create", {task: newTask})
+    .then(response => {
+      setTasks([...tasks,response.data])
+      setTask('')
+    })
+    .catch(error => {
+      alert(error.response ? error.response.data.error.message : error)
+    })
   }
 
+
   const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter(item => item!==deleted)
-    setTasks(withoutRemoved)
+    axios.delete(url + "/delete/" + deleted)
+    .then(response => {
+      setTasks(tasks.filter(item => item.id !== deleted))
+    })
+    .catch(error => {
+      alert(error.response ? error.response.data.error.message : error)
+    })
   }
- 
+
+
   return (
     <div id="container">
       <h3>ToDos</h3>
@@ -36,13 +65,10 @@ function App() {
       <ul>
         {
           tasks.map(item => (
-            <li>
-              {item}
-              <button
-                className='delete-button'
-                onClick={() => deleteTask(item)}>
-                Delete
-              </button>
+            <li key={item.id}>
+              {item.description}
+              <button className='delete-button' onClick={() => 
+                deleteTask(item.id)}>Delete</button>
             </li>
           ))
         }
